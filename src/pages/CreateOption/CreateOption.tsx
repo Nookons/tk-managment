7fc046144813a5bed3a3d1fb93e2e981620cdfbb
TK Side A <apopulargamer@gmail.com>
2024-10-31 11:22:28 +0100

    add menu to create item

--- src/pages/CreateOption/CreateOption.tsx
+++ src/pages/CreateOption/CreateOption.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import {useForm} from "antd/es/form/Form";
-import {Form, Input, Select} from "antd";
+import { useForm } from "antd/es/form/Form";
+import { Form, Input, Select, Space } from "antd";
 import Button from "antd/es/button";
+import InputMask from 'react-input-mask';
+import {db} from "../../firebase";
+import {doc, setDoc} from "firebase/firestore";
 
 const CreateOption = () => {
     const [form] = useForm();
 
-    const onFormFinish = (values: any) => {
-        // todo handle form finish
+    const onFormFinish = async (values: any) => {
+        await setDoc(doc(db, "item_library", values.code), {
+            ...values,
+        });
     };
 
     const onFormFinishFailed = (errorInfo: any) => {
-        // todo handle form finish fail
+        console.log("Failed:", errorInfo);
     };
 
     const onFormClearClick = () => {
@@ -23,29 +28,45 @@ const CreateOption = () => {
             <Form
                 form={form}
                 name="basic"
-                labelCol={{span: 8}}
-                wrapperCol={{span: 16}}
+                labelCol={{ span: 3 }}
+                wrapperCol={{ span: 24 }}
                 layout="horizontal"
-                initialValues={{remember: true}}
+                initialValues={{ remember: true }}
                 onFinish={onFormFinish}
                 onFinishFailed={onFormFinishFailed}
             >
-                <Form.Item wrapperCol={{offset: 0.5, span: 24}}>
-                    <Form.Item label="Select">
-                        <Select>
-                            <Select.Option value="plastic">Plastic</Select.Option>
-                            <Select.Option value="wheel">Wheel</Select.Option>
-                        </Select>
-                    </Form.Item>
-                    <Form.Item label="Input" name="input">
-                        <Input/>
-                    </Form.Item>
-                    <Button type="primary" htmlType="submit">
-                        Submit
-                    </Button>
-                    <Button htmlType="button" onClick={onFormClearClick}>
-                        Clear
-                    </Button>
+                <Form.Item name="type" label="Item type">
+                    <Select>
+                        <Select.Option value="plastic">Plastic</Select.Option>
+                        <Select.Option value="wheel">Wheel</Select.Option>
+                        <Select.Option value="plug">Plug</Select.Option>
+                        <Select.Option value="motor">Motor</Select.Option>
+                        <Select.Option value="button">Button</Select.Option>
+                        <Select.Option value="button_block">Button Block</Select.Option>
+                        <Select.Option value="metal">Metal</Select.Option>
+                        <Select.Option value="other">Other</Select.Option>
+                    </Select>
+                </Form.Item>
+
+                <Form.Item label="Item Name" name="name">
+                    <Input />
+                </Form.Item>
+
+                <Form.Item label="Material Code" name="code">
+                    <InputMask mask="99.99.99999">
+                        {(inputProps: any) => <Input {...inputProps} />}
+                    </InputMask>
+                </Form.Item>
+
+                <Form.Item wrapperCol={{ offset: 3, span: 24 }}>
+                    <Space>
+                        <Button type="primary" htmlType="submit">
+                            Submit
+                        </Button>
+                        <Button htmlType="button" onClick={onFormClearClick}>
+                            Clear
+                        </Button>
+                    </Space>
                 </Form.Item>
             </Form>
         </div>
