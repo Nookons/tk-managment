326a1ac531f9d297dbba3d0c9e76e8fef9d08e4c
TK Side A <apopulargamer@gmail.com>
2024-12-12 11:15:06 +0100

    switch database config to main

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,4 +1,4 @@
-/*export const firebaseConfig = {
+export const firebaseConfig = {
     apiKey: "AIzaSyAaE8hueZhBxgKogifDns-BPGDAoxF09c8",
     authDomain: "tk-servise-web.firebaseapp.com",
     projectId: "tk-servise-web",
@@ -6,7 +6,8 @@
     messagingSenderId: "776717055851",
     appId: "1:776717055851:web:e75e54e53f0e2f29bc7e2e",
     measurementId: "G-K47FV2HPMG"
-};*/
+};
+/*
 export const firebaseConfig = {
     apiKey: "AIzaSyCx85jo-eozEMYqh9YYo1DYy0x4VSrCMgA",
     authDomain: "tk-service-dev.firebaseapp.com",
@@ -15,3 +16,4 @@ export const firebaseConfig = {
     messagingSenderId: "562069912322",
     appId: "1:562069912322:web:1e14d5fdfda89f8abde01b"
 };
+*/
